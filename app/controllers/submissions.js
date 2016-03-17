081dbdb7967b33d215a40d4423de9e4d8d7c3301
rawdan <bogdancrudu@yahoo.com>
2016-03-18 01:40:25 +0200

    display comments

--- app/controllers/submissions.js
+++ app/controllers/submissions.js
@@ -3,8 +3,21 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   title: 'My Blog Submissions',
   body: 'This is the body of my blog submissions',
-  authors: ["Robert", "Michele", "Bruce"]
-
+  authors: ["Robert", "Michele", "Bruce"],
+  comments: [
+    {
+      name: 'Mike Smith',
+      comment: 'Thanks for the great post'
+    },
+    {
+      name: 'John Doe',
+      comment: 'Nice Post!'
+    },
+    {
+      name: 'Jeff Williams',
+      comment: 'Good job!'
+    }
+  ]
 });
 
 
