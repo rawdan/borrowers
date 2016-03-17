2412da0e0a5223445b81efd7467d6aeba9aa1a63
rawdan <bogdancrudu@yahoo.com>
2016-03-17 22:55:42 +0200

    Save current work progress

--- app/router.js
+++ app/router.js
@@ -6,6 +6,15 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('about', {path:'about'});
+  this.route('services', {path:'services'});
+  this.route('contact', {path:'contact'});
+
+  this.resource('posts', {path: '/posts'}, function(){
+    this.route('new');
+    this.route('post', {path:':post_id'});
+  });
+  this.route('events');
 });
 
 export default Router;
